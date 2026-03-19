4ffd9fed018303cd07a89a549b9170d152ff13fe
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-19 17:27:53 +0500

    Add src/components/Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white">
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold">
+          Next.js Blog
+        </Link>
+        <div className="space-x-4">
+          <Link href="/" className="hover:underline">
+            Home
+          </Link>
+          <Link href="/about" className="hover:underline">
+            About
+          </Link>
+          <Link href="/contact" className="hover:underline">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
